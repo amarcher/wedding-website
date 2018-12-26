9dd148433b5e17db600269d8ebf7b924f3a288e0
Andrew Archer <andrew.m.archer@gmail.com>
2018-12-26 14:23:47 -0500

    before ejecting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
+// import Nav from './Nav';
 import './App.css';
+
+// import { XMAS_IN_RI } from './photos/index';
+import BAY_BRIDGE from './photos/bay_bridge.jpeg';
+
+const layers = [{
+  image: BAY_BRIDGE,
+  expanded: true,
+  amount: 0.2,
+  slowerScrollRate: false,
+}];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ParallaxProvider>
+          <header className="App-header">
+            <h1>Andy & Alicia</h1>
+          </header>
+          <main>
+            <div className="hero-container">
+              <ParallaxBanner layers={layers}>
+                <div
+                    className="hero-image"
+                    style={{ backgroundImage: `url(${BAY_BRIDGE})`, backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+                />
+              </ParallaxBanner>
+            </div>
+          </main>
+        </ParallaxProvider>
       </div>
     );
   }
