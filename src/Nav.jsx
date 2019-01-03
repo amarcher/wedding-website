ca61eb3433242f54c317c22496d8106b8c19e1a3
Andrew Archer <andrew.m.archer@gmail.com>
2019-01-02 18:51:58 -0800

    add eslint dependency and NavLink

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,31 +1,18 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './Nav.css';
-
-const propTypes = {
-  match: PropTypes.shape({
-    url: PropTypes.string,
-  }),
-};
-
-const defaultProps = {
-  match: {},
-};
 
 /* eslint-disable-next-line react/prefer-stateless-function */
 class Nav extends Component {
   render() {
-    const { match: { url } } = this.props;
-
     return (
       <div className="nav">
         <nav className="nav__content">
           <ul>
-            <li className="nav__list-item"><Link to={`${url || ''}/`}>Home</Link></li>
-            <li className="nav__list-item"><Link to={`${url || ''}/about`}>About</Link></li>
-            <li className="nav__list-item"><Link to={`${url || ''}/topics`}>Topics</Link></li>
+            <li className="nav__list-item"><NavLink to="/" exact>Home</NavLink></li>
+            <li className="nav__list-item"><NavLink to="/about">Wedding</NavLink></li>
+            <li className="nav__list-item"><NavLink to="/topics">Location</NavLink></li>
           </ul>
         </nav>
         <div className="nav__spacer" />
@@ -34,6 +21,4 @@ class Nav extends Component {
   }
 }
 
-Nav.propTypes = propTypes;
-Nav.defaultProps = defaultProps;
 export default Nav;
