1de33cc71fdcb6b7220d07fd668354af8777672f
Andrew Archer <andrew.m.archer@gmail.com>
2019-03-09 15:05:29 -0800

    add challenges for ssl verification

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', (req, res) => res.send('pong'));
+
+// ssl verification
+app.get(
+  '/.well-known/acme-challenge/98obWujpy49xrPukQYCDRe5qkueRbSsR2WL1ataDNxw',
+  (_req, res) => res.sendFile(path.join(__dirname, '.well-known/acme-challenge/', '98obWujpy49xrPukQYCDRe5qkueRbSsR2WL1ataDNxw')),
+);
+app.get(
+  '/.well-known/acme-challenge/r350cU9NWy271gZMdOgtHJyfUupSCxNgG2EwYWhIzJY',
+  (_req, res) => res.sendFile(path.join(__dirname, '.well-known/acme-challenge/', 'r350cU9NWy271gZMdOgtHJyfUupSCxNgG2EwYWhIzJY')),
+);
+
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 
 app.listen(port);
