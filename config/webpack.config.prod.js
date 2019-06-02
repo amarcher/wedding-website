aaaf0959f06bef2fd8ad03a140c06e8bbf1a5f9f
Andrew Archer <andrew.m.archer@gmail.com>
2019-06-02 08:45:35 -0700

    Bundle size profiles (#7)
    * add webpack bundle analyzer and remove react-scroll-parallax
    
    * add rsvp
    
    * add webpack bundle analyzer and remove react-scroll-parallax
    
    * remove inclusions

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -21,6 +21,7 @@ const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin-alt');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 // Webpack uses `publicPath` to determine where the app is being served from.
@@ -523,6 +524,9 @@ module.exports = {
         silent: true,
         formatter: typescriptFormatter,
       }),
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
+    }),
   ].filter(Boolean),
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
