18593fc962606b64a8b2ef4da81c67775588121a
Andrew Archer <andrew.m.archer@gmail.com>
2019-06-12 15:15:49 -0700

    redirect to https; shrink kerry photo

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use(bodyParser.urlencoded({
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 
+
+app.use((req, res, next) => {
+  if (req.headers.host.indexOf('localhost') > -1 || req.secure) {
+    return next();
+  }
+
+  return res.redirect(res.redirect(['https://', req.get('Host'), req.baseUrl].join('')));
+});
+
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
