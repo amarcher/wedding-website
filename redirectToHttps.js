4dd62046cc78e28c5b2098d0f5a3ad5c55a58825
Andrew Archer <andrew.m.archer@gmail.com>
2019-06-12 17:03:02 -0700

    one more try

--- redirectToHttps.js
+++ redirectToHttps.js
@@ -1,6 +1,6 @@
 function redirectToHttps(req, res, next) {
-  if (!req.headers.host.indexOf('localhost') > -1 && req.protocol === 'https') {
-    res.redirect(res.redirect(['https://', req.get('Host'), req.baseUrl].join('')));
+  if (!req.headers.host.indexOf('localhost') > -1 && req.protocol !== 'https') {
+    return res.redirect(res.redirect(['https://', req.get('Host'), req.baseUrl].join('')));
   }
 
   next();
